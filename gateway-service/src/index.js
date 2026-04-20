1cb40931b6591e9dccb3f7d391392ba922594187
Riyanshi1 <riyanshichaudhary449@gmail.com>
2026-04-20 21:28:26 +0530

    proxy server

--- gateway-service/src/index.js
+++ gateway-service/src/index.js
@@ -1,9 +1,25 @@
-const express = require('express');
+const express = require("express");
+const axios = require("axios");
 const app = express();
 app.use(express.json());
-app.get("/",(req,res)=>{
-    res.send("Gateway Service");
+app.get("/", (req, res) => {
+  res.send("Gateway is running");
 });
-app.listen(5000,()=>{
-    console.log("Gateway Service is running on port 5000");
+app.get("/proxy", async (req, res) => {
+  try {
+    const { url } = req.query;
+    if (!url) {
+      return res.status(400).json({ error: "URL is required" });
+    }
+    const response = await axios.get(url);
+    res.status(response.status).json(response.data);
+  } catch (error) {
+    res.status(500).json({
+      error: "Failed to fetch from target API",
+      details: error.message,
+    });
+  }
+});
+app.listen(5000, () => {
+  console.log("Gateway running on port 5000");
 });
