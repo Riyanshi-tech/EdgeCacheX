6ed399c4031f3349180e39e23027ead4c69b28a5
Riyanshi1 <riyanshichaudhary449@gmail.com>
2026-04-22 22:16:54 +0530

    dynamic gateway proxy

--- gateway-service/src/index.js
+++ gateway-service/src/index.js
@@ -1,25 +1,51 @@
 const express = require("express");
 const axios = require("axios");
+const routes = require("./routes");
+
 const app = express();
 app.use(express.json());
+
 app.get("/", (req, res) => {
-  res.send("Gateway is running");
+  res.send("Gateway running ");
 });
-app.get("/proxy", async (req, res) => {
+
+// DYNAMIC ROUTING
+app.use(async (req, res) => {
   try {
-    const { url } = req.query;
-    if (!url) {
-      return res.status(400).json({ error: "URL is required" });
+    const requestPath = req.path;
+
+    // Find matching route
+    const route = routes.find((r) => requestPath.startsWith(r.prefix));
+
+    if (!route) {
+      return res.status(404).json({ error: "No route found" });
     }
-    const response = await axios.get(url);
+
+    // Remove prefix
+    const newPath = requestPath.replace(route.prefix, "");
+
+    // Construct target URL
+    const targetUrl = route.target + newPath;
+
+    console.log("Forwarding to:", targetUrl);
+
+    // Forward request
+    const response = await axios({
+      method: req.method,
+      url: targetUrl,
+      data: req.body,
+      params: req.query,
+    });
+
     res.status(response.status).json(response.data);
   } catch (error) {
     res.status(500).json({
-      error: "Failed to fetch from target API",
+      error: "Gateway error",
       details: error.message,
     });
   }
 });
+
 app.listen(5000, () => {
   console.log("Gateway running on port 5000");
 });
